bb8ffa5307428d6d4e3fbc63c1e6e49c852728bd
sbaumohl <28550422+sbaumohl@users.noreply.github.com>
2024-11-07 01:07:54 -0600

    finish ind., sep.

---
+++ main.py
@@ -0,0 +1,13 @@
+from manim import *
+from scipy.stats import norm
+import random
+
+
+
+def generate_independence_data():
+    for i in range(500):
+        print(norm.ppf(0.99999))
+
+
+
+generate_independence_data()
